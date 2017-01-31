254b2085aa4dbbfa735faa66112b653590bd4c29
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-01-31 12:55:41 -0500

    handle argv better

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 mod rsh;
 
 use std::env;
+use std::path::PathBuf;
 
 fn main() {
     let mut argv = env::args();
+
+    argv.next();
     
-    let path = argv.
-        next().
-        unwrap_or(".".to_string());
+    let mut path: PathBuf;
+
+    match argv.next() {
+        Some(arg) => path = PathBuf::from(arg),
+        None => path = env::current_dir().unwrap(),
+    };
 
     let mut s = rsh::State::new(path);
     rsh::run(s)
