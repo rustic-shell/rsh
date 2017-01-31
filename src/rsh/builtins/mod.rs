8b095146e93cdc388809bb8818984225ff934133
Mathew Robinson <mathew.robinson3114@gmail.com>
2017-01-31 12:56:42 -0500

    rename util function

--- src/rsh/builtins/mod.rs
+++ src/rsh/builtins/mod.rs
@@ -1,7 +1,9 @@
-use rsh::State;
 use std::path::PathBuf;
 use std::collections::HashMap;
 use std::slice::Iter;
+
+use rsh::utils;
+use rsh::State;
 
 pub type Builtin = fn(State) -> State;
 
@@ -9,7 +11,7 @@ pub fn load() -> HashMap<String, Builtin> {
     let mut h = HashMap::new();
 
     h.insert("cd".to_string(), cd as fn(State) -> State);
-    // h.insert("ls".to_string(), ls as fn(State) -> State);
+    h.insert("ls".to_string(), ls as fn(State) -> State);
 
     h
 }
@@ -17,60 +19,61 @@ pub fn load() -> HashMap<String, Builtin> {
 fn cd(s: State) -> State {
     match s.argv.get(1) {
         Some(x) => {
-            let mut newS = s.clone();
+            let mut new_state = s.clone();
+            let mut new_path = PathBuf::from(x);
 
-            newS.cwd = PathBuf::from(x);
-            return newS
-        },
+            if new_path.has_root() {
+                new_state.cwd = new_path;
+                return new_state;
+            }
+
+
+            match utils::make_absolute(new_path) {
+                Ok(p) => new_state.cwd = p,
+                Err(e) => {
+                    println!("cd: {}", e);
+                    new_state.cwd = s.cwd;
+                }
+            };
+
+
+            new_state
+        }
         None => s.clone(),
     }
 }
 
-//  fn ls<'a>(s: State) -> State {
-//      let dirs: Iter<String>;
+fn ls(s: State) -> State {
+    if s.argv.len() == 1 {
+        list_dir(&s.cwd);
+        return s;
+    }
 
-//      if s.argv.len() == 0 {
-//          let cwd = s.cwd.to_str().unwrap_or("").to_string();
-//          let mut args: &'a Vec<String>;
-//          args = Vec::new();
-//          args.push(cwd);
+    for d in s.argv.iter() {
+        let mut p = PathBuf::from(d);
+        list_dir(&p);
+    }
 
-//          dirs = args.iter();
-//      } else {
-//          let mut args = s.argv.clone().iter();
-//          args.next();
-//          dirs = args;
-//      }
+    s
+}
 
-//      for d in dirs {
-//          let mut p = PathBuf::from(d);
+fn list_dir(p: &PathBuf) {
+    // Cheking if file so we don't do extra processing
+    if p.is_file() {
+        println!("FILE: {}", p.to_str().unwrap_or("WTF"));
+        return;
+    }
 
-//          // Cheking if file so we don't do extra processing 
-//          if p.is_file() {
-//              println!("{}", p.to_str().unwrap_or(""));
-//              continue;
-//          }
+    // Unwrapping because we know it's a dir, not a file
+    for entry in p.read_dir().unwrap() {
+        match entry {
+            Ok(e) => {
+                // TODO replace this unwrap to something safer
+                print!("{} ", e.file_name().into_string().unwrap());
+            }
+            Err(e) => println!("Error: {}", e),
+        }
+    }
 
-//          // Unwrapping because we know it's a dir, not a file 
-//          for entry in p.read_dir().unwrap() {
-//              match entry {
-//                  Ok(e) => {
-//                      // TODO replace this unwrap to something safer
-//                      println!("{}", e.file_name().into_string().unwrap());
-//                  },
-//                  Err(e) => println!("Error: {}", e),
-
-//              }
-//          }
-//      }
-
-//      s
-// }
-
-
-
-
-
-
-
-
+    print!("\n");
+}
